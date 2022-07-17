a1d7043dcabb80ec43f1a84083d87c8314bb5fc6
timberlakent <Timberlake.NT@gmail.com>
2022-07-17 13:50:47 -0400

    created more circles and added a start and stop to the timer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,51 @@
-const button1 = document.getElementById("#circle1");
+//constants/lets/var
+let buttonOne = document.querySelector("#circle-one");
+let buttonTwo = document.querySelector("#circle-two");
+// let buttonThree = document.querySelector("#circle-three");
+// let buttonFour = document.querySelector("#circle-four");
+// let buttonFive= document.querySelector("#circle-five");
 
-// timer logic
-const startingMinutes = 0;
-let time = startingMinutes * 60;
+let lastButton = document.querySelector("#circle-last");
 
 const countDown = document.getElementById("countdown");
 
-setInterval(updateCountdown, 1000);
+//hide buttons until press
+buttonTwo.style.display="none"
+// buttonThree.style.display="none"
+// buttonFour.style.display="none"
+// buttonFive.style.display="none"
+lastButton.style.display="none"
+//add an on click event listener that starts the game and timer.
+buttonOne.addEventListener('click', event =>{
+        console.log("hello")
+       clear = setInterval(updateCountdown, 1000);
+        event.target.remove();
+        buttonTwo.style.display="inline";
+});
 
+buttonTwo.addEventListener('click', event => {
+    event.target.remove()
+    lastButton.style.display="inline";
+})
+
+lastButton.addEventListener('click', event =>{
+    event.target.remove();
+    clearInterval(clear)
+    
+    
+
+})
+
+
+// timer logic
+//create starting timer
+const startingMinutes = 0;
+//create starting  minutes 
+let time = startingMinutes * 60;
+clear;
+
+
+//write a function that creats a timer 
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
